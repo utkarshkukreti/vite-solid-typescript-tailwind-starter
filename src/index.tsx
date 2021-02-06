1a0abd11814872357f9ca062d594ae02d5ba44d1
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2021-02-06 12:07:22 +0530

    add nice interactive demo content

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,29 @@
 import './index.css'
 
+import { createSignal, onCleanup } from 'solid-js'
 import { render } from 'solid-js/web'
 
-const App = () => <div class="text-center m-16 text-4xl">Hello!</div>
+const App = () => {
+  const [counter, setCounter] = createSignal(0)
+  const [increment, setIncrement] = createSignal(1)
+  const interval = setInterval(
+    () => setCounter(counter() + increment()),
+    1000 / 60,
+  )
+  onCleanup(() => clearInterval(interval))
+  return (
+    <div class="m-16 flex flex-col justify-center items-center">
+      <div className="text-5xl">Counter: {counter}</div>
+      <div className="mb-8"></div>
+      <button
+        className="rounded-none text-xl border-b border-dashed border-black"
+        onClick={() => setIncrement(increment() * 2)}
+      >
+        Click here to speed up!
+      </button>
+    </div>
+  )
+}
 
 const element = document.querySelector('main')
 
