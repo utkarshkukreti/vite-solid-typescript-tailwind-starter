5770b374a0c8252642dfcbcf632f2cac8814d90e
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2021-02-06 11:58:56 +0530

    better name for this

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,10 @@ import { render } from 'solid-js/web'
 
 const App = () => <div class="text-center m-16 text-4xl">Hello!</div>
 
-const main = document.querySelector('main')
+const element = document.querySelector('main')
 
-if (main) {
-  const dispose = render(() => <App />, main)
+if (element) {
+  const dispose = render(() => <App />, element)
   if (import.meta.hot) {
     import.meta.hot?.accept()
     import.meta.hot?.dispose(dispose)
